67d4493c63d1a1f93c95aa2cbe5f7bfcf05f660d
witch <janeflowerss@protonmail.com>
2023-05-21 03:51:04 -0400

    ENTITY class

---
+++ lua/packages/class-war/entity.lua
@@ -0,0 +1,57 @@
+local Create
+Create = ents.Create
+local Register
+Register = scripted_ents.Register
+local lower, Left
+do
+  local _obj_0 = string
+  lower, Left = _obj_0.lower, _obj_0.Left
+end
+local TraceLine
+TraceLine = util.TraceLine
+local logger = _PKG:GetLogger()
+local gm = gmod.GetGamemode()
+do
+  local _class_0
+  local _base_0 = { }
+  _base_0.__index = _base_0
+  _class_0 = setmetatable({
+    __init = function() end,
+    __base = _base_0,
+    __name = nil
+  }, {
+    __index = _base_0,
+    __call = function(cls, ...)
+      local _self_0 = setmetatable({}, _base_0)
+      cls.__init(_self_0, ...)
+      return _self_0
+    end
+  })
+  _base_0.__class = _class_0
+  local self = _class_0
+  self.__inherited = function(self, child)
+    local fields
+    do
+      local _tbl_0 = { }
+      for k, v in pairs(child) do
+        if Left(k, 2) ~= "__" then
+          _tbl_0[k] = v
+        end
+      end
+      fields = _tbl_0
+    end
+    fields.Base = fields.Base or self.__barcode
+    fields.Spawnable = fields.Spawnable or self.Spawnable
+    fields.Category = fields.Category or self.Category
+    child.__barcode = tostring(self.__barcode and self.__barcode .. '/' or '') .. tostring(lower(rawget(child, '__entity') or child.__name))
+    Register(fields, child.__barcode)
+    logger:Debug("+ENTITY: " .. tostring(child.__barcode))
+    if CLIENT and gm.IsSandboxDerived and child.Spawnable then
+      return timer.Create(tostring(_PKG), 1, 1, function()
+        logger:Debug('Reloading the spawnmenu.')
+        return RunConsoleCommand('spawnmenu_reload')
+      end)
+    end
+  end
+  return _class_0
+end
---
+++ lua/packages/class-war/entity.moon
@@ -0,0 +1,20 @@
+import Create from ents
+import Register from scripted_ents
+import lower, Left from string
+import TraceLine from util
+logger  = _PKG\GetLogger!
+gm      = gmod.GetGamemode!
+class
+    @__inherited: (child) =>
+        fields = {k,v for k,v in pairs(child) when Left(k, 2) != "__"}
+        fields.Base or= @__barcode
+        fields.Spawnable or= @Spawnable
+        fields.Category or= @Category
+        child.__barcode = "#{@__barcode and @__barcode .. '/' or ''}#{lower(rawget(child, '__entity') or child.__name)}"
+        Register fields, child.__barcode
+        logger\Debug "+ENTITY: #{child.__barcode}"
+        if CLIENT and gm.IsSandboxDerived and child.Spawnable
+            timer.Create tostring(_PKG), 1, 1, ->
+                logger\Debug 'Reloading the spawnmenu.'
+                RunConsoleCommand 'spawnmenu_reload'
+    --new: => @ = Create @__barcode
