c88e01464f21efa0befd1c7865a15cbc63637a91
witch <janeflowerss@protonmail.com>
2023-05-21 03:51:24 -0400

    metadata

---
+++ lua/packages/class-war/package.lua
@@ -0,0 +1,18 @@
+name = "class-war"
+main = "init.lua"
+cl_main = nil
+version = 0
+client = true
+server = true
+menu = false
+autorun = true
+isolation = true
+send = {
+  "entity.lua",
+  "uplink.lua",
+  "examples.lua"
+}
+logger = true
+gamemodes = nil
+maps = nil
+singleplayer = false
